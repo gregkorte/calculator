6b18abf1f13f552a820b071edd38dd9de2dae4df
thatknitter <kblewis.flt@gmail.com>
2014-11-07 15:21:14 -0600

    click operators

--- js/main.js
+++ js/main.js
@@ -1,8 +1,61 @@
+$(function(){
+	var $numButton = $("input.number");
+	var $opButton = $("operator");
+	var inputOp = $opButton.val();
+	var $rightBut = $("input.rightbut");
+	var inputRight = $rightBut.val();
+	var $display = $("displaybar");
+	$numButton.on('click', function(){
+		$('#displayoutput').val($(this).val());
+		//append the numbers
+	});
+	$rightBut.on('click', function(){
+		if($(this).val() === 'clear'){
+			//clear the display bar
+		} else {
+			//run the equals operator
+		}
+	});
+	
+});
 function displayOutput(){
-	return $('#displayOutput').val();
+	return $('#displayoutput').val();
 }
 
+var prior = 0;
+
 function press(buttonValue){
-	var current = $('#displaoyoutput').val();
+	switch (buttonValue){
+		case "+":
+		  prior += +$('#displayoutput').val();
+		  $('#displayoutput').val('');
+		  //handle +
+		  break;
+		case "-":
+		//handle -
+		break;
+		case "*":
+		//handle *
+		break;
+		case "/":
+		//handle /
+		break;
+		case "=":
+		  prior += +$('#displayoutput').val();
+		  $('#displayoutput').val(prior);
+		  prior = 0;
+		//handle =
+		break;
+		case "C":
+		//handle C
+		break;
+		case "+/-":
+		//handle +/-
+		break;
+	
+	default:
+	//handle numbers
+	current = $('#displayoutput').val();
 	$('#displayoutput').val(current += buttonValue);
-}
+	}
+};
